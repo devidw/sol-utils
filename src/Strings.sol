91f63f7a2b3166160a3732efe8c578d5f977224f
David Wolf <68775926+devidw@users.noreply.github.com>
2022-10-22 19:38:44 +0000

    update Strings.sol and Strings.t.sol

--- src/Strings.sol
+++ src/Strings.sol
@@ -9,14 +9,43 @@ library Strings {
     {
         bytes memory _inArr = bytes(_str);
         uint256 _index;
-        if (_pos >= 0) {
-            _index = uint256(_pos);
-        } else {
-            _index = uint256(int256(_inArr.length) + _pos);
-        }
-        require(_inArr.length > _index, "Position out of bounds");
+        _index = _translateIndex(_pos, _inArr.length);
         bytes memory _outArr = new bytes(1);
         _outArr[0] = _inArr[_index];
         _char = string(_outArr);
+    }
+
+    function slice(
+        string memory _str,
+        int256 _start,
+        int256 _end
+    ) internal pure returns (string memory out) {
+        bytes memory _inArr = bytes(_str);
+        uint256 _startPos;
+        uint256 _endPos;
+        _startPos = _translateIndex(_start, _inArr.length);
+        _endPos = _translateIndex(_end, _inArr.length);
+        require(
+            _startPos <= _endPos,
+            "Start position must be less than end position"
+        );
+        bytes memory _outArr = new bytes(_endPos - _startPos);
+        for (uint256 i = _startPos; i < _endPos; i++) {
+            _outArr[i - _startPos] = _inArr[i];
+        }
+        out = string(_outArr);
+    }
+
+    function _translateIndex(int256 _index, uint256 _length)
+        private
+        pure
+        returns (uint256 _translated)
+    {
+        if (_index < 0) {
+            _translated = uint256(int256(_length) + _index);
+        } else {
+            _translated = uint256(_index);
+        }
+        require(_translated < _length, "Position out of bounds");
     }
 }
--- test/Strings.t.sol
+++ test/Strings.t.sol
@@ -8,11 +8,6 @@ contract StringsTest is Test {
     using Strings for string;
     string private _str = "abc";
 
-    function testCannotGetCharThatIsOutOfBounds() public {
-        vm.expectRevert("Position out of bounds");
-        _str.charAt(3);
-    }
-
     function testGetCharAtPositivePos() public {
         assertEq(_str.charAt(0), "a");
         assertEq(_str.charAt(1), "b");
@@ -23,5 +18,32 @@ contract StringsTest is Test {
         assertEq(_str.charAt(-1), "c");
         assertEq(_str.charAt(-2), "b");
         assertEq(_str.charAt(-3), "a");
+    }
+
+    function testCannotGetCharThatIsOutOfBounds() public {
+        vm.expectRevert("Position out of bounds");
+        _str.charAt(3);
+    }
+
+    function testSlice() public {
+        assertEq(_str.slice(0, 1), "a");
+        assertEq(_str.slice(-3, 1), "a");
+        assertEq(_str.slice(0, 2), "ab");
+        assertEq(_str.slice(-3, -1), "ab");
+    }
+
+    function testStartSliceOutOfBounds() public {
+        vm.expectRevert("Position out of bounds");
+        _str.slice(10, 1);
+    }
+
+    function testEndSliceOutOfBounds() public {
+        vm.expectRevert("Position out of bounds");
+        _str.slice(0, 10);
+    }
+
+    function testStartSliceGreaterThanEndSlice() public {
+        vm.expectRevert("Start position must be less than end position");
+        _str.slice(1, 0);
     }
 }
