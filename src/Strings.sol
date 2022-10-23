6e1ff78effbde25830c5cd11a09994d0443dd39b
David Wolf <68775926+devidw@users.noreply.github.com>
2022-10-23 14:56:59 +0000

    update devcontainer.json, Strings.sol and Strings.t.sol

--- src/Strings.sol
+++ src/Strings.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.13;
 
 library Strings {
+    function length(string memory _str) internal pure returns (uint256) {
+        return bytes(_str).length;
+    }
+
     function charAt(string memory _str, int256 _pos)
         internal
         pure
@@ -21,10 +25,9 @@ library Strings {
         int256 _end
     ) internal pure returns (string memory out) {
         bytes memory _inArr = bytes(_str);
-        uint256 _startPos;
-        uint256 _endPos;
-        _startPos = _translateIndex(_start, _inArr.length);
-        _endPos = _translateIndex(_end, _inArr.length);
+        uint256 _inArrLen = _inArr.length;
+        uint256 _startPos = _translateIndex(_start, _inArrLen);
+        uint256 _endPos = _translateIndex(_end, _inArrLen);
         require(
             _startPos <= _endPos,
             "Start position must be less than end position"
@@ -47,5 +50,61 @@ library Strings {
             _translated = uint256(_index);
         }
         require(_translated < _length, "Position out of bounds");
+    }
+
+    function ltrim(string memory _in) internal pure returns (string memory) {
+        bytes memory _inArr = bytes(_in);
+        uint256 _inArrLen = _inArr.length;
+        uint256 _start;
+        // Find the index of the first non-whitespace character
+        for (uint256 i = 0; i < _inArrLen; i++) {
+            bytes1 _char = _inArr[i];
+            if (
+                _char != 0x20 && // space
+                _char != 0x09 && // tab
+                _char != 0x0a && // line feed
+                _char != 0x0D && // carriage return
+                _char != 0x0B && // vertical tab
+                _char != 0x00 // null
+            ) {
+                _start = i;
+                break;
+            }
+        }
+        bytes memory _outArr = new bytes(_inArrLen - _start);
+        for (uint256 i = _start; i < _inArrLen; i++) {
+            _outArr[i - _start] = _inArr[i];
+        }
+        return string(_outArr);
+    }
+
+    function rtrim(string memory _in) internal pure returns (string memory) {
+        bytes memory _inArr = bytes(_in);
+        uint256 _inArrLen = _inArr.length;
+        uint256 _end;
+        // Find the index of the last non-whitespace character
+        for (uint256 i = _inArrLen - 1; i >= 0; i--) {
+            bytes1 _char = _inArr[i];
+            if (
+                _char != 0x20 && // space
+                _char != 0x09 && // tab
+                _char != 0x0a && // line feed
+                _char != 0x0D && // carriage return
+                _char != 0x0B && // vertical tab
+                _char != 0x00 // null
+            ) {
+                _end = i;
+                break;
+            }
+        }
+        bytes memory _outArr = new bytes(_end + 1);
+        for (uint256 i = 0; i <= _end; i++) {
+            _outArr[i] = _inArr[i];
+        }
+        return string(_outArr);
+    }
+
+    function trim(string memory _in) internal pure returns (string memory) {
+        return ltrim(rtrim(_in));
     }
 }
--- test/Strings.t.sol
+++ test/Strings.t.sol
@@ -7,6 +7,20 @@ import "../src/Strings.sol";
 contract StringsTest is Test {
     using Strings for string;
     string private _str = "abc";
+    bytes private _whitespaces = new bytes(6);
+
+    function setUp() public {
+        _whitespaces[0] = 0x20;
+        _whitespaces[1] = 0x09;
+        _whitespaces[2] = 0x0a;
+        _whitespaces[3] = 0x0D;
+        _whitespaces[4] = 0x0B;
+        _whitespaces[5] = 0x00;
+    }
+
+    function testLength() public {
+        assertEq(_str.length(), 3);
+    }
 
     function testGetCharAtPositivePos() public {
         assertEq(_str.charAt(0), "a");
@@ -45,5 +59,36 @@ contract StringsTest is Test {
     function testStartSliceGreaterThanEndSlice() public {
         vm.expectRevert("Start position must be less than end position");
         _str.slice(1, 0);
+    }
+
+    function testLeftTrim() public {
+        string memory _trimStr = string.concat(string(_whitespaces), "a");
+        assertEq(_trimStr.ltrim(), "a");
+    }
+
+    function testLeftTrimWithNoWhitespace() public {
+        assertEq(_str.ltrim(), _str);
+    }
+
+    function testRightTrim() public {
+        string memory _trimStr = string.concat("a", string(_whitespaces));
+        assertEq(_trimStr.rtrim(), "a");
+    }
+
+    function testRightTrimWithNoWhitespace() public {
+        assertEq(_str.rtrim(), _str);
+    }
+
+    function testTrim() public {
+        string memory _trimStr = string.concat(
+            string(_whitespaces),
+            "a",
+            string(_whitespaces)
+        );
+        assertEq(_trimStr.trim(), "a");
+    }
+
+    function testTrimWithNoWhitespace() public {
+        assertEq(_str.trim(), _str);
     }
 }
